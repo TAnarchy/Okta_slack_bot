829d2bea707e7fe0d9db4d63483fd3844f773c8c
Glitch (greet-bot) <none>
2020-07-18 04:33:16 +0000

    🌪😂 Checkpoint ./okta_connector.js:19297790/1549

--- okta_connector.js
+++ okta_connector.js
@@ -2,34 +2,14 @@ const https = require('https');
 const okta_url=process.env.OKTA_URL
 const okta_token=process.env.OKTA_TOKEN
 const okta_path=process.env.OKTA_PATH
-exports.getUsers = () => {
-  
-  var headers={"Authorization":"SSWS 00O5uxkxKYooyJEJZMgqDaahNdCaFK15AQi7ZqZ9Pp"}
-  
-  var options = {
-  hostname: okta_url,
-  path: okta_path,
-  method: 'GET',
-  headers:headers
-};
-  
-  var blank=""
-var req = https.request(options, function(res) {
-  console.log("statusCode: ", res.statusCode);
-  console.log("headers: ", res.headers);
- 
-  res.on('data', function(d) {
-    blank="not"
-    return "Success"
-  });
-});
-req.end();
+const okta = require('@okta/okta-sdk-nodejs');
 
-req.on('error', function(e) {
-  blank="not"
-  return "failure"
+const okta_client = new okta.Client({
+  orgUrl: okta_url,
+  token: "00O5uxkxKYooyJEJZMgqDaahNdCaFK15AQi7ZqZ9Pp"    // Obtained from Developer Dashboard
 });
- 
-  while(blank=""){}
-  
+
+exports.getUsers = () => {
+  var users=okta_client.listUsers()
+  return JSON.stringify(users);
 }
