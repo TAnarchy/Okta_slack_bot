d43208c545419470fc7b1ae76fc8e892aec51054
Glitch (greet-bot) <none>
2020-07-18 04:21:32 +0000

    🌿🗃 Checkpoint ./okta_connector.js:19297790/206 ./index.js:19297790/1483

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.message('knock knock', async ({ message, say }) => {
 
 app.message('list', async ({ message, say }) => {
   var userList = await okta_connect.getUsers()
-  say(`List is ${userList}`);
+  say(`List is ${okta_connect.getUsers()}`);
 });
 
 // Start your app
