8a86d18c4e152073afd832c1e6e748fe6658642b
Glitch (greet-bot) <none>
2020-07-18 03:24:47 +0000

    👂🎅 Checkpoint ./okta_connector.js:19297790/753

--- okta_connector.js
+++ okta_connector.js
@@ -1,3 +1,24 @@
-getUsers = () => {
+const https = require('https');
+const okta_url=process.env.OKTA_URL
+const okta_token=process.env.OKTA_TOKEN
+exports.getUsers = () => {
+  
+  https.get(okta_url, (resp) => {
+  let data = '';
+
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(JSON.parse(data).explanation);
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
+  
   return "Users"
 }
