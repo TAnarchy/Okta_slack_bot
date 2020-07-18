b46dfef45800fd132d33cab265545da9a5d74f2f
Glitch (greet-bot) <none>
2020-07-18 06:56:27 +0000

    🚤☃️ Checkpoint ./okta_connector.js:19297790/708

--- okta_connector.js
+++ okta_connector.js
@@ -3,11 +3,12 @@ const okta_url=process.env.OKTA_URL
 const okta_token=process.env.OKTA_TOKEN
 const okta_path=process.env.OKTA_PATH
 const okta = require('@okta/okta-sdk-nodejs');
+const req = require('sync-request');
 
 
 
 exports.getUsers = () => {
-  try{
+  /*try{
     const okta_client = new okta.Client({
       orgUrl: "https://moveworkseddie-admin.okta.com",
       token: "00O5uxkxKYooyJEJZMgqDaahNdCaFK15AQi7ZqZ9Pp"    // Obtained from Developer Dashboard
@@ -22,7 +23,15 @@ exports.getUsers = () => {
   }
   catch(e){
     return e
-  }
+  }*/
+  
+  var userRes=req("GET",okta_url+okta_path,{
+    headers :{
+      'Authorization':'SSWS 00O5uxkxKYooyJEJZMgqDaahNdCaFK15AQi7ZqZ9Pp'
+    }
+  })
+  
+  return userRes.getBody("utf-8")
 }
 
 
